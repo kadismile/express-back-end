4a51333f537ac85b4ec7545e7344021138c7037e
kadismile <brianking319@gmail.com>
2019-12-16 10:57:19 +0100

    added jwt and cookies to application

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {
-  registerUser, login} = require('../controllers/authController');
+  registerUser, login, getMe} = require('../controllers/authController');
+
+const { protect } = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
